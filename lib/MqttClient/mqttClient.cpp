c0311d31083f1c86bcb0c8bd91c8829792410539
Geraldo Júnior <geraldo.junior@ifood.com.br>
2022-11-27 13:52:44 -0300

    adjustis

--- lib/MqttClient/mqttClient.cpp
+++ lib/MqttClient/mqttClient.cpp
@@ -5,11 +5,11 @@
 #include "../Log/logs.h"
 
 // WiFi
-const char *ssid = "telegestao"; // Enter your WiFi name
-const char *password = "telegestao@password";  // Enter WiFi password
+const char *ssid = "GalaxyS20";    // Enter your WiFi name
+const char *password = "afak1247"; // Enter WiFi password
 
 // MQTT Broker
-const char *mqtt_broker = "192.168.1.9";
+const char *mqtt_broker = "192.168.89.180";
 const char *topic = "teste";
 const char *mqtt_username = "esp";
 const char *mqtt_password = "esp";
@@ -18,45 +18,52 @@ const int mqtt_port = 1883;
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-void callback(char *topic, byte *payload, unsigned int length) {
+void callback(char *topic, byte *payload, unsigned int length)
+{
     loginfo("Message arrived in topic: ");
     loginfo(topic);
     loginfo("Message:");
 
-    for (int i = 0; i < length; i++) {
-        loginfo((char*) payload[i]);
+    for (int i = 0; i < length; i++)
+    {
+        loginfo((char *)payload[i]);
     }
 
     loginfo("");
     loginfo("-----------------------");
 }
 
-void initMqttClient() {
+void initMqttClient()
+{
     WiFi.begin(ssid, password);
 
-    while (WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED)
+    {
         delay(500);
         loginfo("Connecting to WiFi..");
     }
 
-    //connecting to a mqtt broker
+    // connecting to a mqtt broker
     client.setServer(mqtt_broker, mqtt_port);
     client.setCallback(callback);
 
     String host = WiFi.getHostname();
-    while (!client.connected()) {
+    while (!client.connected())
+    {
 
         String client_id = "esp32-client-";
         client_id += String(WiFi.macAddress());
         Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
 
-        if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+        if (client.connect(client_id.c_str(), mqtt_username, mqtt_password))
+        {
             Serial.println("Public emqx mqtt broker connected");
-        } else {
+        }
+        else
+        {
             Serial.print("failed with state ");
             Serial.print(client.state());
             delay(2000);
         }
- }
-
+    }
 }
